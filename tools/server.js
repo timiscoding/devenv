9ca889f7b62ba19719544759ef282228985acc69
Tim <timiscoding@gmail.com>
2017-06-26 22:49:09 +1000

    Fix sass middleware paths

--- tools/server.js
+++ tools/server.js
@@ -12,7 +12,7 @@ browserifyMiddleware.settings({
 });
 
 const sassConfig = {
-  src: path.join(__dirname, '../scss'),
+  src: path.join(__dirname, '../src/scss'),
   dest: path.join(__dirname, '../dist'),
   debug: true
 };
@@ -20,7 +20,7 @@ const sassConfig = {
 app.use(sassMiddleware(sassConfig));
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('/bundle.js', browserifyMiddleware(path.join(__dirname, '../src/js/main.js'))); 
+app.get('/bundle.js', browserifyMiddleware(path.join(__dirname, '../src/js/main.js')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
