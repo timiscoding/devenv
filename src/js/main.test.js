f12472fff0601a735cf0b55d4cb20258471ff14a
Tim <timiscoding@gmail.com>
2017-06-27 23:21:48 +1000

    jsdom for enzyme mount rendering

--- src/js/main.test.js
+++ src/js/main.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { expect } from 'chai';
-import Hi from './main';
+import Main from './main';
 
 describe('main.js', () => {
   it('tests true', () => {
     expect(true).to.eql(true);
   });
 
-  it('renders <Hi />', () => {
-    const wrapper = shallow(<Hi />);
+  it('renders <Main />', () => {
+    const wrapper = mount(<Main />);
     expect(wrapper.find('div').exists()).to.be.true;
   });
 });
